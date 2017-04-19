4e6475e9b361022bee13a146b75b465d674b7a62
Konstantin Barkalov <mail@barkalov.ru>
2017-04-20 05:59:02 +0700

    SSHA2 ready

---
+++ test/test_fail.js
@@ -0,0 +1,20 @@
+var should = require('should')
+var ldapHash = require('../lib/ldapHash')
+var crypto = require('crypto')
+
+describe('bad things (must fail)',function(){
+    describe('bad mode',function(){
+        it('should not be checkable',function(done){
+                let passwd='pwd', 
+                    hash='{SSHB}c6AhsUGD7NfYyTofZoKiuP5MDqjAcKGi';
+                ldapHash.checkHash(passwd, hash).then((result)=> {
+                    should.not.exist(result);
+                    done();
+                }).catch((err)=> {
+                    should.exist(err);
+                    err.message.should.equal('unsupported mode: SSHB');
+                    done();
+                });
+        })
+    })
+})
